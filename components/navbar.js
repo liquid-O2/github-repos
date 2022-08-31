b27fb1e5fc3283b7c0062046a9b5e06295857947
liquid-O2 <this.is.arunava.b@gmail.com>
2022-09-01 01:57:44 +0530

    some scss changes

---
+++ components/navbar.js
@@ -0,0 +1,23 @@
+import Link from 'next/link';
+
+const Navbar = () => {
+  return (
+    <nav className='navbar mt-1 text-primary'>
+      <div className='container'>
+        <Link href='/'>
+          <a>Github Battle</a>
+        </Link>
+        <div className='right-links display-f justify-space-between'>
+          <Link href='/popular'>
+            <a className='pr-1'>Popular</a>
+          </Link>
+          <Link href='Battle'>
+            <a>Battle</a>
+          </Link>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,16 @@
 import Head from 'next/head';
 import Image from 'next/image';
-
+import Link from 'next/link';
+import Navbar from '../components/navbar';
 export default function Home() {
   return (
-    <div className='container'>
+    <>
       <Head>
         <title>Github Battle</title>
         <meta name='description' content='Github battle' />
       </Head>
-    </div>
+      <Navbar />
+      <div className='container'></div>
+    </>
   );
 }
